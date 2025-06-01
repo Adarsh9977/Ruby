9ef54ce39ffb052dd8885130298d618de115358f
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-06-01 16:30:10 +0530

    variables

---
+++ variables.rb
@@ -0,0 +1,11 @@
+name = "Kirat"
+age = 27
+name2 = "Ruby"
+age2 = 29
+
+puts ("hello " + name + "!")
+puts ("how are you " + name + "?")
+puts ("kirat is " + age.to_s + " years old")
+puts ("hello " + name2 + "!")
+puts ("how are you " + name2 + "?")
+puts ( name2 + " is " + age2.to_s + " years old")
