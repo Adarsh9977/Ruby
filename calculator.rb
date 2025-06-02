aa396679c110697eef0affcc861201ee684f1b2a
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-06-02 12:59:29 +0530

    Implement basic calculator functionality and add conditional weather output

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,23 @@
- 
+puts "Enter the first number"
+a = gets.chomp.to_f
+puts "Enter the operator (+, -, *, /)"
+operator = gets.chomp
+puts "Enter the second number"
+b = gets.chomp.to_f
+
+
+if operator == "+"
+    puts "The result is #{a + b}"
+elsif operator == "-"
+    puts "The result is #{a - b}"
+elsif operator == "*"
+    puts "The result is #{a * b}"
+elsif operator == "/"
+    if b != 0
+        puts "The result is #{a / b}"
+    else
+        puts "Error: Division by zero is not allowed."
+    end
+else
+    puts "Error: Invalid operator. Please use +, -, *, or /."
+end
---
+++ if-else.rb
@@ -0,0 +1,12 @@
+cloudy = false
+rainy = false
+sunny = false
+
+
+if cloudy
+  puts "It's cloudy today."
+elsif rainy
+  puts "It's rainy today."
+else
+  puts "It's sunny today."
+end
